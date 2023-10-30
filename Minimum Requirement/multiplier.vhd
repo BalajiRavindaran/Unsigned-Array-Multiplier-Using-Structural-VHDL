c3c56b14bcdb139ebbb087ca318443c527172c15
Nitish Sundarraj <sundarrajnithish@gmail.com>
2023-10-30 17:22:54 -0400

    Some Files Update

--- Minimum Requirement/multiplier.vhd
+++ Minimum Requirement/multiplier.vhd
@@ -2,3 +2,13 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+entity program_counter is
+	port(
+		clk: in std_logic;
+        address_in: in std_logic_vector(31 downto 0);
+
+        current_address: out std_logic_vector(31 downto 0);
+		next_address: out std_logic_vector(31 downto 0)
+        
+	);
+end program_counter;
