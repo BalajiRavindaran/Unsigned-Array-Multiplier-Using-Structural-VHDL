ec7342216c206975cb634d57529ff5397d437c14
Balaji Ravindaran <balaji05.ravindaran@gmail.com>
2023-11-11 22:59:13 -0500

    [11/11/2023] - Complete Test Bench for divider unit (Min. Req.)

--- Minimum Requirement/tb_divider_unit.vhd
+++ Minimum Requirement/tb_divider_unit.vhd
@@ -1,63 +1,35 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity divider_unit_test_bench is
 end divider_unit_test_bench;
 
 architecture behavior of divider_unit_test_bench is
-    signal A: std_logic_vector(15 downto 0);
-    signal D: std_logic_vector(15 downto 0);
+    signal A : std_logic_vector(15 downto 0);
+    signal D : std_logic_vector(15 downto 0);
 
     component divider_unit is
         port (
             A : in std_logic_vector(15 downto 0);
-            D: out std_logic_vector(15 downto 0)
+            D : out std_logic_vector(15 downto 0)
         );
     end component;
 
+    constant MAX : natural := 65535;
 begin
-    DU_test: divider_unit port map (A => A, D => D);
+    DU_test : divider_unit port map (A => A, D => D);
 
     process
     begin
-        -- Test case 1 (65534 / 4)
-        A <= "1111111111111110"; -- 65534
-        wait for 10 ns;
-        assert (D = "0011111111111110") -- 16382
-            report "Test case 1 failed" severity error;
-
-        -- Test case 2 (65535 / 4)
-        A <= "1111111111111111"; -- 65535
-        wait for 10 ns;
-        assert (D = "0011111111111111") -- 1
-            report "Test case 2 failed" severity error;
-
-        -- Test case 3 (52699 / 4)
-        A <= "1100111001111011"; -- 52699
-        wait for 10 ns;
-        assert (D = "0011001101110110") -- 13174
-            report "Test case 3 failed" severity error;
-
-        -- Test case 4 (0 / 4)
-        A <= "0000000000000000"; -- 0
-        wait for 10 ns;
-        assert (D = "0000000000000000") -- 0
-            report "Test case 4 failed" severity error;
-        
-        -- Test case 5 (36969 / 4)
-        A <= "1001000000111001"; -- 36969
-        wait for 10 ns;
-        assert (D = "10010000011010") -- 9242
-            report "Test case 5 failed" severity error;
-
-        -- Test case 6 (32923 / 4)
-        A <= "0111111111111011"; -- 32923
-        wait for 10 ns;
-        assert (D = "10000000100110") -- 8230
-            report "Test case 6 failed" severity error;
-
-        
-        -- Stop simulation
+        for i in MAX downto 0 loop
+            A <= std_logic_vector(to_unsigned(i, A'length));
+            wait for 1 ns;
+            assert D = std_logic_vector(to_unsigned(i/4, D'length))
+                report "Test case " & integer'image(i) & " Failed" severity error;
+        end loop;
         wait;
     end process;
 
