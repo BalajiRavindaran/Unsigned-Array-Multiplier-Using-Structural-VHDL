96d7eef5ca0cbc1c0cab2c8aadbd5fda6960b4ee
Nitish Sundarraj <sundarrajnithish@gmail.com>
2023-11-17 10:51:09 -0500

    Temporary Files

---
+++ Additional Requirements/wallace_tree_multiplier.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity wallace_tree_multiplier is
+    port(
+    A : in std_logic_vector(7 downto 0);
+    B : in std_logic_vector(7 downto 0);
+
+    P : out std_logic_vector(15 downto 0)
+    );
+end wallace_tree_multiplier;
+
+architecture behavior of wallace_tree_multiplier is
+    
+
+    component full_adder
+        port (
+            A, B, Cin: in std_logic;
+            S, Cout: out std_logic
+        );
+    end component;
+
+    component half_adder
+        port (
+            A, B: in std_logic;
+            S, Cout: out std_logic
+        );
+    end component;
+
+    component multiplier
+        port (
+            A, B: in std_logic;
+            C: out std_logic
+        );
+    end component;
---
+++ Minimum Requirement/flip_flop.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity jk_flip_flop is
+    port (
+        j, k, clk, reset: in std_logic;
+        q, q_bar: out std_logic
+    );
+end entity jk_flip_flop;
+
+architecture structural of jk_flip_flop is
+    
+end architecture structural;
