1729e1190e23eb8fb273c99b038c9c554d17c0ee
Nitish Sundarraj <sundarrajnithish@gmail.com>
2023-12-04 11:17:19 -0500

    2^C Divider Done!

--- Additional Requirement v2/adder_d.vhd
+++ Additional Requirement v2/adder_d.vhd
@@ -1,34 +1,56 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL; 
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity generic_adder is
-    generic (N: INTEGER:= 8);
-    port (
-    A, B: in std_logic_vector(N-1 downto 0);
-    P: out std_logic_vector(N-1 downto 0)
-    );
-end generic_adder;
+entity GenericAdder is
+  generic (
+    N : integer := 8   -- Default bit width
+  );
+  port (
+    A, B : in std_logic_vector(2*N-1 downto 0);
+    P : out std_logic_vector(2*N downto 0)
+  );
+end GenericAdder;
 
-architecture beh_arch of generic_adder is
+architecture Behavioral of GenericAdder is
+
     component full_adder is
         port(
             A : in std_logic;
             B : in std_logic;
             Cin : in std_logic;
-    
             S : out std_logic;
             Cout : out std_logic
         );
- 	end component;
+    end component;
 
-    signal carry: std_logic;
-    signal sum: std_logic_vector(N-1 downto 0);
+    type signal_array is array (natural range <>, natural range <>) of std_logic;
 
-    begin
-        f00: full_adder port map (Cin=>'0', A=>A(0), B=>B(0), S=>sum(0), Cout=>carry);
-        horizontal_stack: for i in 1 to N-1 generate
-            fij: full_adder port map (Cin=>carry, A=>A(i), B=>B(i), S=>sum(i), Cout=>carry);
+    signal ab : signal_array(N-1 downto 0, N-1 downto 0);
+    signal sum : signal_array(N downto 0, N downto 0);
+    signal carry : signal_array(N downto 0, N-1 downto 0);
+
+begin
+    generate_adder: for i in 0 to N-1 generate
+        generate_adder_stage: for j in 0 to N-1 generate
+            signed_independent: if ((i < N-1 and j = N-1) or (i = N-1 and j < N-1)) generate
+                ab(i, j) <= A(i) xor B(j);
+            end generate;
+
+            adder_instance: if (i < N-1 or (i = N-1 and j = N-1)) generate
+                fa: full_adder port map (
+                    Cin => carry(i, j-1) when j > 0 else '0',
+                    A => sum(i, j-1) when j > 0 else '0',
+                    B => ab(i, j),
+                    S => sum(i, j),
+                    Cout => carry(i, j)
+                );
+            end generate;
         end generate;
+    end generate;
 
-        P <= sum;
-end beh_arch;
+    P <= '0' & sum(N-1, N-1) when N > 0 else (others => '0');  -- Output the sum
+
+end Behavioral;
+
